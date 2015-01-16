bb115e2524e6b79b0b2bd5fc219112392b854f07
lishiyo <c.ziye.li@gmail.com>
2015-01-16 19:31:38 +0000

    switch back to 02

--- app/helpers/assets_util.rb
+++ app/helpers/assets_util.rb
@@ -3,5 +3,7 @@ module AssetsUtil
     Dir.glob(Rails.root.join("app/assets/images/**/*.*")).map do |path| 
       path.gsub(Rails.root.join("app/assets/images/").to_s, "")
     end
+
+# 		["background-01.jpg", "background-02.jpg", "background-03.jpg", "default_avatar_01.jpg", "default_avatar_02.png", "default_avatar_03.png"]
   end
 end
