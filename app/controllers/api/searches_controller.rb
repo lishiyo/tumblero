6fd792813fa671efbb5a3773f7938ead887598fa
lishiyo <c.ziye.li@gmail.com>
2015-02-02 02:28:11 +0000

    stops guest posts and blogs from showing up in explore

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,8 +1,7 @@
 class Api::SearchesController < ApplicationController
 	
-	def index
-	
-		# SINGLE SEARCH through a dashboard or blog
+	# SINGLE SEARCH through a dashboard or blog
+	def index			
 		query = params[:query] 
 
 		if params[:blog_id]
@@ -19,9 +18,11 @@ class Api::SearchesController < ApplicationController
 	# SINGLE SEARCH through index of all blogs that are not yours
 	def blogs
 		if params[:query] && params[:query].empty?
-			@search_results = Blog.where.not(user_id: current_user.id).page(params[:page]).per(4)
+			@search_results = Blog.where(guest: false).where.not(user_id: current_user.id).page(params[:page]).per(4)
 		else
-			@search_results = Blog.where.not(user_id: current_user.id).search_by_tags(params[:query]).page(params[:page]).per(4)
+			@search_results = Blog.where(guest: false)
+														.where.not(user_id: current_user.id)
+														.search_by_tags(params[:query]).page(params[:page]).per(4)
 		end
 		
 		render 'single_search'
@@ -30,9 +31,9 @@ class Api::SearchesController < ApplicationController
 	# SINGLE SEARCH through index of all posts (10 per page)
 	def posts
 		if params[:query] && params[:query].empty?
-			@search_results = Post.includes(:taggings).page(params[:page])
+			@search_results = Post.where(guest: false).includes(:taggings).page(params[:page])
 		else
-			@search_results = Post.includes(:taggings).search_by_tags(params[:query]).page(params[:page])
+			@search_results = Post.where(guest: false).includes(:taggings).search_by_tags(params[:query]).page(params[:page])
 		end
 		
 		render 'single_search'
@@ -44,7 +45,7 @@ class Api::SearchesController < ApplicationController
 		query = (params[:term] || params[:query])
 		
     @search_results = PgSearch
-		.multisearch(query)
+			.multisearch(query)
 			.page(params[:page])
 		
 		render 'multi_search'
