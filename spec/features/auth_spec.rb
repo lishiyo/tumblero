668751f717caa4fbb7e5d5aff5cd8f8ccc2445c0
lishiyo <c.ziye.li@gmail.com>
2015-03-31 22:21:45 +0000

    testing push to codeship

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -4,11 +4,11 @@ feature "the sign-up process", js: true do
 	
 	it "has a new user page" do
 		visit "/"
-		expect(page).to have_content('sign up')
+		expect(page).to have_content('Sign Up')
 	end
 	
 	feature "signing up a user" do
-		# 	let(:user) { FactoryGirl.build(:user) }
+			let(:user) { FactoryGirl.build(:user) }
 		
 		it "redirects to new blog page upon success" do
 			
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
 require 'capybara/rails'
-require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
